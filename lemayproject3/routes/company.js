ce396cffea0ad3c7a8432faebd9a3c79695b006e
Aidan LeMay <aidanpathways12@gmail.com>
2021-12-04 23:56:27 -0500

    Adding CompanyDelete

--- lemayproject3/routes/company.js
+++ lemayproject3/routes/company.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-const bl = require('../BusinessLayer/departmentVal.js');
 var DataLayer = require("../companydata/index.js");
 var dl = new DataLayer("ahl4753");
 
 module.exports = require('../companydata/lib/DataLayer');
 
-/* GET department */
-router.get('/company/:company', function(req, res, next) {
-    // 
+// DELETE DEPARTMENT | TEST LAST
+router.delete('/company', function(req, res, next) {
+    var response;
+    if ((req.query.company).length < 1) {
+        response = {
+            error: "Department Not Found"
+        }
+    }
+    else {
+        del = dl.deleteCompany(req.query.company);
+        if (del > 0) {
+            response = {
+                success: "Company Successfully Deleted"
+            }
+        }
+        else {
+            response = {
+                error: "Company Delete Failed"
+            }
+        }
+        
+    }
+    return response;
 });
 
 module.exports = router;
